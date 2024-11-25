51aada804911a1c021bf39c951f820979360af2f
SiddharthaShukla8 <151515728+SiddharthaShukla8@users.noreply.github.com>
2024-11-25 16:23:52 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://chat-app-backend-flame-gamma.vercel.app/';
+  axios.defaults.baseURL = 'https://chatappbackend-1-m64x.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
